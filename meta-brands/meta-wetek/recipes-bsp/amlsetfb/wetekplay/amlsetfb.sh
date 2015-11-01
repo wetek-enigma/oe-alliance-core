b541c02f3b14d6ed2df66b6526fd6cdf799eecd2
Captain <captain.onboard@web.de>
2015-11-01 17:05:33 +0100

    update build update amlfbset stop load bpsplash

--- meta-brands/meta-wetek/recipes-bsp/amlsetfb/wetekplay/amlsetfb.sh
+++ meta-brands/meta-wetek/recipes-bsp/amlsetfb/wetekplay/amlsetfb.sh
@@ -80,14 +80,14 @@ esac
 echo 0 > /sys/class/graphics/fb0/blank
 echo 1 > /sys/class/graphics/fb1/blank
 
-pbsplash
+#pbsplash
 /usr/bin/remotecfg /etc/amremote/wetek.conf
 
 echo rm default > /sys/class/vfm/map
 echo add default decoder ppmgr deinterlace amvideo > /sys/class/vfm/map
 #echo 912000 >   /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-#echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-#echo 1200000 >  /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1200000 >  /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 ln -sf /sys/class/leds/wetek\:blue\:powerled/brightness /dev/led0
 ln -sf /sys/class/leds/wetek\:blue\:ethled/brightness /dev/led1
 ln -sf /sys/class/leds/wetek\:blue\:wifiled/brightness /dev/led2
